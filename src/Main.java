10d35998a188416980aacc9d6275bbd61a58cc6c
EV-135 <velasqen@mail.uc.edu>
2024-09-23 12:38:57 -0400

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,9 @@
+public class Main {
+    public static void main(String[] args) {
+        int evenNum = 4;
+        if (evenNum % 2 ==0)
+            System.out.println("The number is even");
+        else
+            System.out.println("The number is odd");
+    }
+}
